3e61b09926eabb7d3a013ec4c278155e77ca6fe8
Dmitrii <inaumovd@gmail.com>
2023-07-16 10:59:08 +0800

    adding favorite empty page / layout

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,6 +7,7 @@ import OfferPage from 'src/pages/offer';
 import Page404 from 'src/pages/404';
 import PrivateRoute, {AuthorizationStatus} from './private-route';
 import FavoritesPage from '../pages/favorites';
+import Layout from '../components/layout/layout.tsx';
 
 export enum AppRoute {
   Main='/',
@@ -19,12 +20,21 @@ const Router: React.FC = () => (
   <Routes>
     <Route path={AppRoute.Main} element={<MainPage/>} />
     <Route path={AppRoute.Login} element={<LoginPage/>} />
-    <Route path={AppRoute.Favorites} element={
-      <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
-        <FavoritesPage/>
-      </PrivateRoute>
-    }
-    />
+    {/*<Route path={AppRoute.Favorites} element={*/}
+    {/*  <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>*/}
+    {/*    <FavoritesPage/>*/}
+    {/*  </PrivateRoute>*/}
+    {/*}*/}
+    {/*/>*/}
+    <Route path={AppRoute.Favorites} element={<Layout/>}>
+      <Route index element={
+        <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
+          <FavoritesPage/>
+        </PrivateRoute>
+      }
+      />
+    </Route>
+
     <Route path={`${AppRoute.Offer}/:offerId`} element={<OfferPage/>} />
     <Route path='*' element={<Page404/>} />
   </Routes>
