e99dde96434a12e8b71e1667d50467b62c0037ac
Dmitrii <inaumovd@gmail.com>
2023-07-16 12:03:00 +0800

    adding feedback form

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,6 +2,7 @@ import {ReactElement} from 'react';
 import {NavLink, Outlet} from 'react-router-dom';
 
 import {AppRoute} from 'src/router';
+import {AuthorizationStatus} from 'src/router/private-route';
 
 interface LayoutProps {
   withFooter?: boolean;
@@ -10,6 +11,7 @@ interface LayoutProps {
 function Layout(props: LayoutProps): ReactElement {
 
   const {withFooter} = props;
+  const authStatus: AuthorizationStatus = AuthorizationStatus.Auth;
 
   return (
     <div className="page">
@@ -28,25 +30,40 @@ function Layout(props: LayoutProps): ReactElement {
               </NavLink>
             </div>
             <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="#"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
+              {
+                authStatus === AuthorizationStatus.Auth &&
+                  <ul className="header__nav-list">
+                    <li className="header__nav-item user">
+                      <a
+                        className="header__nav-link header__nav-link--profile"
+                        href="#"
+                      >
+                        <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                        <span className="header__user-name user__name">
                   Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
+                        </span>
+                        <span className="header__favorite-count">3</span>
+                      </a>
+                    </li>
+                    <li className="header__nav-item">
+                      <a className="header__nav-link" href="#">
+                        <span className="header__signout">Sign out</span>
+                      </a>
+                    </li>
+                  </ul>
+              }
+              {
+                authStatus !== AuthorizationStatus.Auth &&
+                <ul className="header__nav-list">
+                  <li className="header__nav-item user">
+                    <a className="header__nav-link header__nav-link--profile" href="#">
+                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                      <span className="header__login">Sign in</span>
+                    </a>
+                  </li>
+                </ul>
+              }
+
             </nav>
           </div>
         </div>
